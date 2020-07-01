51debab0d1a942a356969599aa4af36a1fab1b36
Moisés Jiménez Martín <moises.jimenez@rindus.de>
2020-07-01 15:59:50 +0200

    Excercise 1 Part 1

--- exercise/problem/requests.go
+++ exercise/problem/requests.go
@@ -4,9 +4,12 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
+
 	sites := []string{
 		"https://www.google.com",
 		"https://drive.google.com",
@@ -15,12 +18,19 @@ func main() {
 	}
 
 	for _, site := range sites {
+		wg.Add(1)
+
 		go func(site string) {
+			defer wg.Done()
+
 			res, err := http.Get(site)
+
 			if err != nil {
 			}
 
 			io.WriteString(os.Stdout, res.Status+"\n")
 		}(site)
 	}
+
+	wg.Wait()
 }
